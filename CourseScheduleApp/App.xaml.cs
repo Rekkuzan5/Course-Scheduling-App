c56e3040b414dc8f5258715c5e40a9632136d19e
Rekkuzan5 <cstapp88@gmail.com>
2023-01-17 20:37:03 -0600

    Add project files.

---
+++ CourseScheduleApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CourseScheduleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var dashboard = new Dashboard();
+            var navPage = new NavigationPage(dashboard);
+            MainPage = navPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
